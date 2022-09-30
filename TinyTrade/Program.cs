6e54fc8059a0bd0e528f85b4879a3086381938e9
Matteo Beltrame <bltmtt@gmail.com>
2022-09-30 20:10:01 +0200

    Initial commit

---
+++ TinyTrade/Program.cs
@@ -0,0 +1,14 @@
+﻿using HandierCli;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using TinyTrade.Services;
+
+Console.Title = "TinyTrade";
+Logger.ConsoleInstance.LogInfo("----- NFT GENERATOR -----\n\n", ConsoleColor.DarkCyan);
+
+var host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+        services.AddHostedService<CommandLineService>())
+    .Build();
+
+await host.RunAsync();
