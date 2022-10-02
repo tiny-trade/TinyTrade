d21eee5a8e640b554cf645ab00b6ffa0ffdd7657
Matteo Beltrame <bltmtt@gmail.com>
2022-10-03 01:02:18 +0200

    Strategies and indicators modules

---
+++ TinyTrade.Strategies/SampleStrategy.cs
@@ -0,0 +1,23 @@
+﻿using TinyTrade.Core.Constructs;
+using TinyTrade.Core.Exchanges;
+using TinyTrade.Core.Strategy;
+
+namespace TinyTrade.Strategies;
+
+public class SampleStrategy : AbstractStrategy
+{
+    public SampleStrategy(StrategyConstructorParameters parameters) : base(parameters)
+    {
+    }
+
+    protected override float GetStakeAmount() => 0F;
+
+    protected override float GetStopLoss(OrderSide side, DataFrame frame) => 0F;
+
+    protected override float GetTakeProfit(OrderSide side, DataFrame frame) => 0F;
+
+    protected override void Tick(DataFrame frame)
+    {
+        base.Tick(frame);
+    }
+}
