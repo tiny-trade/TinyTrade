1acb6f0e69707c53e37e277346778907afb2d63c
Matteo Beltrame <bltmtt@gmail.com>
2022-10-04 19:00:41 +0200

    Dummy link

--- TinyTrade.Live/Program.cs
+++ TinyTrade.Live/Program.cs
@@ -1,5 +1,9 @@
 ﻿// args passed: "mode" "strategy file" "pair"
 
+using TinyTrade.Strategies.Link;
+
+TinyTradeStrategiesAssembly.DummyLink();
+
 Console.WriteLine(args);
 await Task.Delay(10000);
 Console.WriteLine("Dying");
