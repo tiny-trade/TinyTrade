050db59475fb48416af9722b86fbee3aa0e66a6f
Matteo Beltrame <bltmtt@gmail.com>
2022-10-07 00:34:44 +0200

    Bundle archives and gitignore

--- win-x64.bat
+++ win-x64.bat
@@ -1,1 +1,3 @@
-dotnet publish -c Release -r win-x64 -f net6.0 --no-self-contained
+dotnet publish -c Release -r win-x64 -f net6.0 --no-self-contained -o bundle/release/win-x64
+echo Creating archive...
+tar.exe -a -c -f ./win-x64-bundle.zip -C bundle/release/win-x64 *
